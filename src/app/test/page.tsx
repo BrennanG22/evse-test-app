0fe39b4874e78473c491cc9bd8db6f506f646f88
Brennan Giroux <jbrgiroux@gmail.com>
2025-02-26 09:49:04 -0700

    Added vscode to get ignore

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -9,13 +9,17 @@ const ParentComp = () => {
     
     const [currentState, setCurrentState] = useState<number | null>(null);
     const [current, setCurrent] = useState<number |null>(null);
+    const [voltage, setVoltage] = useState<number | null>(null);
+    const [powerLimit, setPowerLimit] = useState<number | null>(null);
+    const [currentPower, setCurrentPower] = useState<number | null>(null);
+
     return(
         <div className="w-full flex">
             <MonitorContainer name="Auth Settings">
                 <AuthBox currentState={currentState}/>
             </MonitorContainer>
             <MonitorContainer name="Stats">
-              <StatsBox current={current}/>
+              <StatsBox current={current} voltage={voltage} powerLimit={powerLimit} currentPower={currentPower}/>
             </MonitorContainer>
         </div>
     )
--- src/app/test/statsBox.tsx
+++ src/app/test/statsBox.tsx
@@ -1,14 +1,17 @@
 interface StatsBoxProps {
   current: number | null;
+  voltage: number | null;
+  powerLimit: number | null;
+  currentPower: number | null;
 }
 
-const StatsBox: React.FC<StatsBoxProps> = ({ current }) => {
+const StatsBox: React.FC<StatsBoxProps> = ({ current, voltage, powerLimit, currentPower}) => {
   return (
     <div className="grid grid-cols-2 gap-4">
-      <h1>Current: </h1>
-      <h1>Voltage: </h1>
-      <h1>Power Limit: </h1>
-      <h1>Current Power: </h1>
+      <h1>Current: {current}</h1>
+      <h1>Voltage: {voltage}</h1>
+      <h1>Power Limit: {powerLimit}</h1>
+      <h1>Current Power: {currentPower}</h1>
     </div>
   );
 }
