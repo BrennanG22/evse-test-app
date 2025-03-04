21f7f77d0abe3915f9233a54643e20c861fc53ce
Brennan Giroux <jbrgiroux@gmail.com>
2025-03-03 18:19:14 -0700

    Added connection method for modbus

--- src/app/modbusConfig/ruleTable.tsx
+++ src/app/modbusConfig/ruleTable.tsx
@@ -2,20 +2,39 @@
 
 import { JSX, useState } from "react";
 import RuleBar from "./ruleBar";
-import { randomUUID } from "crypto";
 
 const RuleTable = () => {
   
   const [rules, setRules] = useState<JSX.Element[]>([]);
 
+  //Connection details
+  const [ipAddress, setIpAddress] = useState<string>("");
+  const [port, setPort] = useState<string>("");
+  const [clientId, setClientId] = useState<string>("");
+
   function addRule(){
     setRules((prevRules) => [...prevRules, <RuleBar key={Date.now()}/>])
+  }
+
+  function startConnection(){
+    fetch("http://localhost:2000/modbus/getModbusConnection", {
+      method:"POST",
+      body: JSON.stringify({
+        address: ipAddress,
+        port: port,
+        id: clientId
+      })
+    });
   }
   
   return(
     <div>
       <div className="flex space-x-3">
         <button onClick={addRule}>Add Rule</button>
+        <button onClick={startConnection}>Start Connection</button>
+        <input placeholder="Ip Address" value={ipAddress} onChange={e => setIpAddress(e.target.value)}></input>
+        <input placeholder="Port" value={port} onChange={e => setPort(e.target.value)}></input>
+        <input placeholder="Client Id" value={clientId} onChange={e => setClientId(e.target.value)}></input>
         <button>Save Rules</button>
       </div>
       <div>
