53c45bfff72405a46a40d9ed0146878177bc5f76
Brennan Giroux <jbrgiroux@gmail.com>
2025-03-05 16:40:32 -0700

    Lots2

--- src/app/modbusConfig/ruleTable.tsx
+++ src/app/modbusConfig/ruleTable.tsx
@@ -18,12 +18,14 @@ const RuleTable = () => {
 
   
   const [rulesDisplay, setRulesDisplay] = useState<JSX.Element[]>([]);
-  const [rules, setRules] = useState<ruleData[]>([]);
+  const [rules, setRules] = useState<ruleData[]>(() => {
+    return typeof window !== "undefined" ? JSON.parse(localStorage.getItem("rules") || "[]") : []});
 
   //Connection details
   const [ipAddress, setIpAddress] = useState<string>(() => {return typeof window !== "undefined" ? localStorage.getItem("ip") || "" : ""});
   const [port, setPort] = useState<string>(() => {return typeof window !== "undefined" ? localStorage.getItem("port") || "" : ""});
   const [clientId, setClientId] = useState<string>(() => {return typeof window !== "undefined" ? localStorage.getItem("clientId") || "" : ""});
+  
 
   function addRule(){
     const tempRule: ruleData = {
@@ -38,8 +40,18 @@ const RuleTable = () => {
     setRules((prevRules) => {const newRules = prevRules.concat(tempRule);
       return newRules;
     });
-    setRulesDisplay((prevRules) => [...prevRules, <RuleBar key={tempRule.id} data={tempRule} updateRuleCallback={updateRuleData}/>]);
+    // setRulesDisplay((prevRules) => [...prevRules, <RuleBar key={tempRule.id} data={tempRule} updateRuleCallback={updateRuleData}/>]);
+    updateRuleRender()
+    
+  }
 
+  function updateRuleRender(){
+    console.log("Refresh Preformed")
+    const tempRulesDisplay:JSX.Element[] = [];
+    rules.forEach((rule) => {
+      tempRulesDisplay.push(<RuleBar key={rule.id} data={rule} updateRuleCallback={updateRuleData}/>)
+    })
+    setRulesDisplay(tempRulesDisplay);
   }
 
   function updateRuleData(data: ruleData){
@@ -60,22 +72,37 @@ const RuleTable = () => {
     localStorage.setItem("ip", ipAddress);
     localStorage.setItem("port", port);
     localStorage.setItem("clientId", clientId);
-  }, [ipAddress, port, clientId])
+    localStorage.setItem("rules", JSON.stringify(rules));
+  }, [ipAddress, port, clientId, rules])
 
   function saveRules(){
-
+    rules.forEach((rule) => {
+      const url = rule.type === "read"? "localhost:2000/modbus/setRecurringReadAPICallback" : 
+      "localhost:2000/modbus/setRecurringWriteAPIGather";
+      fetch(url, {
+        method:"POST",
+        body: JSON.stringify({
+          interval: rule.interval,
+          address: rule.modbusAddress,
+          endpoint: rule.apiEndpoint
+        })
+      })
+    });
   }
 
   function startConnection(){
     fetch("http://localhost:2000/modbus/getModbusConnection", {
       method:"POST",
+      headers:{
+        'Content-Type': 'application/json',
+      },
       body: JSON.stringify({
         address: ipAddress,
         port: port,
         id: clientId
-      })
-    });
-    console.log(rules);
+      }),
+    })
+    .catch((e) => console.log(e));
   };
   
   return(
@@ -86,7 +113,7 @@ const RuleTable = () => {
         <input placeholder="Ip Address" value={ipAddress} onChange={e => setIpAddress(e.target.value)}></input>
         <input placeholder="Port" value={port} onChange={e => setPort(e.target.value)}></input>
         <input placeholder="Client Id" value={clientId} onChange={e => setClientId(e.target.value)}></input>
-        <button>Save Rules</button>
+        <button onClick={saveRules}>Save Rules</button>
       </div>
       <div className="overflow-scroll flex-1">
         {rulesDisplay}
